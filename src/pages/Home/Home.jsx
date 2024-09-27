058356cfa9a4fbf8017ceb850dd38a174fae8a58
Mikael Hadler <mikaelhadler@gmail.com>
2024-09-26 21:14:28 -0300

    include user state

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,15 @@
-import { useReducer } from "react";
 import { Spinner } from "../../components/Spinner/Spinner";
 import { useUser } from "../../contexts/WithReducer/reducers/userReducer";
 
-let count = 0;
 export function Home() {
   const { user, loading, error } = useUser();
 
   return (
     <div className="app">
-
+      <h1>Home</h1>
+      {loading && <Spinner />}
+      {error && <p>Error: {error.message}</p>}
+      {user && <p>User: {user.name}</p>}
     </div>
   );
 }
